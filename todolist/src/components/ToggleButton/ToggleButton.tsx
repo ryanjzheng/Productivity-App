2d5a285f5cd02a02c54aca060c55f16d2b35c4d3
ryanjzheng <rzheng25@terpmail.umd.edu>
2024-09-17 20:37:34 -0400

    fixed build error

--- todolist/src/components/ToggleButton/ToggleButton.tsx
+++ todolist/src/components/ToggleButton/ToggleButton.tsx
@@ -1,6 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { useNavigate } from "react-router-dom";
 import styles from "./ToggleButton.module.css";
 
 interface ToggleButtonProps {
@@ -8,18 +7,12 @@ interface ToggleButtonProps {
 }
 
 const ToggleButton: React.FC<ToggleButtonProps> = ({ onClick }) => {
-  const navigate = useNavigate();
-
-  const handleNavigateHome = () => {
-    navigate('/today')
-  }
 
   return (
     <div className={styles.websiteHeader}>
       <button className={styles.toggleButton} onClick={onClick}>
         <FontAwesomeIcon icon={faBars} className={styles.icon} />
       </button>
-      {/* <div className={`${styles.websiteName}`} onClick={handleNavigateHome}>UPA</div> */}
     </div>
   );
 };
